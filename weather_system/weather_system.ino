db2c768bdfe5f61d1ad4b44bc3b5ea0ac9a311bb
sambhav143 <sambhavjain4441@gmail.com>
2026-09-20 08:08:28 +0530

    weather_system.ino

--- weather_system/weather_system.ino
+++ weather_system/weather_system.ino
@@ -1,17 +1,3 @@
-/*
- * Weather Station: DHT11 + Rain Sensor + BMP280
- * NO LCD VERSION
- *
- * Wiring:
- * - DHT11 Data  -> Pin D4 
- * - Rain Sensor -> Pin A0 (Analog)
- * - BMP280 SDA  -> Pin D2 (GPIO 4)
- * - BMP280 SCL  -> Pin D1 (GPIO 5)
- * - BMP280 VCC  -> 3.3V (IMPORTANT: Do not use 5V)
- * - BMP280 GND  -> GND
- */
-
-// --- BLYNK CONFIGURATION ---
 #define BLYNK_TEMPLATE_ID "TMPL33jOkQQ5E"
 #define BLYNK_TEMPLATE_NAME "Weather monitoring system"
 #define BLYNK_AUTH_TOKEN "B2e8vNSPYRRx0AIAH4vgLaA97oASlaay"
@@ -23,26 +9,19 @@
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
 
-// --- WIFI CREDENTIALS ---
-char ssid[] = "Galaxy S25 Ultra";    // Your WiFi network name
-char pass[] = "12345687";   // Your WiFi password
+char ssid[] = "Galaxy S25 Ultra";
+char pass[] = "12345687";
 
-// --- SENSOR CONFIGURATION ---
-
-// 1. DHT Sensor
-#define DHTPIN D5      // Pin D5
-#define DHTTYPE DHT11  // Change to DHT22 if needed
+#define DHTPIN D5 
+#define DHTTYPE DHT11 
 DHT dht(DHTPIN, DHTTYPE);
 
-// 2. BMP280 Sensor (I2C)
 Adafruit_BMP280 bmp; 
 
-// 3. Rain Sensor
 const int rainPin = A0;
 
 BlynkTimer timer;
 
-// --- FUNCTION TO READ AND SEND DATA ---
 void sendSensorData()
 {
   Serial.println("-----------------------");
@@ -61,7 +40,6 @@ void sendSensorData()
   }
 
   // 2. Read BMP280 (Pressure)
-  // We use a check to see if pressure is valid (non-zero)
   float pressure = bmp.readPressure();
   
   if (pressure > 0) {
@@ -74,7 +52,6 @@ void sendSensorData()
 
   // 3. Read Rain Sensor
   int rainRaw = analogRead(rainPin);
-  // Map 1024 (Dry) -> 0 (Wet) to 0% -> 100%
   int rainPercent = map(rainRaw, 1024, 0, 0, 100); 
   rainPercent = constrain(rainPercent, 0, 100); 
 
@@ -87,14 +64,10 @@ void setup()
   Serial.begin(9600);
   Serial.println("\nStarting Weather Station...");
 
-  // Connect to Blynk
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
   
-  // Start Sensors
   dht.begin();
   
-  // Initialize BMP280
-  // Try generic address 0x76 first, then Adafruit address 0x77
   if (!bmp.begin(0x76)) {
     if (!bmp.begin(0x77)) {
       Serial.println("Error: Could not find BMP280 sensor!");
@@ -106,7 +79,6 @@ void setup()
     Serial.println("Success: BMP280 found at 0x76");
   }
 
-  // Timer: Run the function every 2 seconds
   timer.setInterval(2000L, sendSensorData);
 }
 
