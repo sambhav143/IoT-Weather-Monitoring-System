6c161110dd28e0dd043e13d671616a35c4f8f1e0
sambhav143 <sambhavjain4441@gmail.com>
2026-09-20 08:09:43 +0530

    weather_system.ino

--- weather_system/weather_system.ino
+++ weather_system/weather_system.ino
@@ -26,36 +26,33 @@ void sendSensorData()
 {
   Serial.println("-----------------------");
 
-  // 1. Read DHT (Temp & Humidity)
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
     Serial.println("Error: Failed to read from DHT sensor!");
   } else {
-    Blynk.virtualWrite(V0, t); // V0 for Temperature
-    Blynk.virtualWrite(V1, h); // V1 for Humidity
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
     Serial.print("Temp: "); Serial.print(t); Serial.print("C  ");
     Serial.print("Hum: "); Serial.print(h); Serial.println("%");
   }
 
-  // 2. Read BMP280 (Pressure)
   float pressure = bmp.readPressure();
   
   if (pressure > 0) {
-    pressure = pressure / 100.0; // Convert Pascals to hPa
-    Blynk.virtualWrite(V3, pressure); // V3 for Pressure
+    pressure = pressure / 100.0;
+    Blynk.virtualWrite(V3, pressure);
     Serial.print("Pressure: "); Serial.print(pressure); Serial.println(" hPa");
   } else {
     Serial.println("Error: BMP280 not connected or reading 0");
   }
 
-  // 3. Read Rain Sensor
   int rainRaw = analogRead(rainPin);
   int rainPercent = map(rainRaw, 1024, 0, 0, 100); 
   rainPercent = constrain(rainPercent, 0, 100); 
 
-  Blynk.virtualWrite(V2, rainPercent); // V2 for Rainfall
+  Blynk.virtualWrite(V2, rainPercent);
   Serial.print("Rain: "); Serial.print(rainPercent); Serial.println("%");
 }
 
